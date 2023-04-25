3d126ccced402f131f0270a0f525c1952bb02f87
Yusuf Aslan <ttyusuf18@gmail.com>
2023-04-25 16:44:31 +0300

    v1.0 Start

--- src/test/java/automationExercise/AutomationExercise_04.java
+++ src/test/java/automationExercise/AutomationExercise_04.java
@@ -1,8 +1,29 @@
 package automationExercise;
 
+import org.junit.Test;
 import utilities.TestBase;
 
 public class AutomationExercise_04 extends TestBase {
+    /*
+1. Launch browser
+2. Navigate to url 'http://automationexercise.com'
+3. Verify that home page is visible successfully
+4. Click on 'Signup / Login' button
+5. Verify 'Login to your account' is visible
+6. Enter correct email address and password
+7. Click 'login' button
+8. Verify that 'Logged in as username' is visible
+9. Click 'Logout' button
+10. Verify that user is navigated to login page
+     */
+    @Test
+    public void test_04(){
+       // 2. Navigate to url 'http://automationexercise.com'
+        driver.get("http://automationexercise.com");
+
+        //3. Verify that home page is visible successfully
+
+    }
 
 
 }
