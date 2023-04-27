0558afe2c7244d0922fbd93b4b1583da085863bd
Salih Karagoz <salihkaragoz068@gmail.com>
2023-04-28 02:27:02 +0300

    v1.1SK

--- src/test/java/automationExercise/AutomationExercise_07.java
+++ src/test/java/automationExercise/AutomationExercise_07.java
@@ -1,14 +1,31 @@
 package automationExercise;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import utilities.TestBase;
 
+import static org.junit.Assert.assertTrue;
+
 public class AutomationExercise_07 extends TestBase {
-      //  1. Tarayıcıyı başlatın
-      //  2. 'http://automationexercise.com' URL'sine gidin
+
     @Test
     public void test07 (){
+        //  1. Tarayıcıyı başlatın
+        //  2. 'http://automationexercise.com' URL'sine gidin
         driver.get("http://automationexercise.com");
+
+        // 3. Ana sayfanın başarıyla göründüğünü doğrulayın
+        WebElement anaSayfa = driver.findElement(By.xpath("//body"));
+        assertTrue(anaSayfa.isDisplayed());
+
+        // 4. 'Test Durumları' düğmesine tıklayın
+        driver.findElement(By.xpath("(//button[@type='button'][1])")).click();
+
+        // 5. Kullanıcının test senaryoları sayfasına başarıyla yönlendirildiğini doğrulayın
+
+       WebElement testSenoryalarıSayfası = driver.findElement(By.xpath("//body[@style]"));
+       assertTrue(testSenoryalarıSayfası.isDisplayed());
 
 
     }
