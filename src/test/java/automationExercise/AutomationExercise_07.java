70e228b38cd7a1b331650a56c262c708a74060de
Salih Karagoz <salihkaragoz068@gmail.com>
2023-04-28 01:19:13 +0300

    v1

---
+++ src/test/java/automationExercise/AutomationExercise_07.java
@@ -0,0 +1,17 @@
+package automationExercise;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class AutomationExercise_07 extends TestBase {
+      //  1. Tarayıcıyı başlatın
+      //  2. 'http://automationexercise.com' URL'sine gidin
+    @Test
+    public void test07 (){
+        driver.get("http://automationexercise.com");
+
+
+    }
+
+
+}
